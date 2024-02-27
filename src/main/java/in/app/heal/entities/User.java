cb3303c6e81650c71dc2562ae6a880bbda253b8e
Yash <yashrtalele@gmail.com>
2024-02-27 16:58:32 +0530

    Added entities - Comments and FlaggedPosts

--- src/main/java/in/app/heal/entities/User.java
+++ src/main/java/in/app/heal/entities/User.java
@@ -20,25 +20,26 @@ public class User {
   @Column(name = "user_id")
   private Integer user_id;
 
-  @Column(name = "first_name", nullable = false)
-  private String first_name;
+  @Column(name = "first_name", nullable = false) private String first_name;
 
-  @Column(name = "last_name", nullable = false)
-  private String last_name;
+  @Column(name = "last_name", nullable = false) private String last_name;
 
   @Column(name = "contact_number", nullable = false)
   private Long contact_number;
 
-  @OneToMany(mappedBy = "user_id", cascade = CascadeType.ALL, orphanRemoval = true)
+  @OneToMany(mappedBy = "user_id", cascade = CascadeType.ALL,
+             orphanRemoval = true)
   private List<JournalEntry> journalEntries;
 
-  @OneToMany(mappedBy = "user_id", cascade = CascadeType.ALL, orphanRemoval = true)
+  @OneToMany(mappedBy = "user_id", cascade = CascadeType.ALL,
+             orphanRemoval = true)
   private List<Blogs> blogs;
-
-  @OneToMany(mappedBy = "user_id", cascade = CascadeType.ALL, orphanRemoval = true)
-  private List<Votes> votes;
 
   @OneToMany(mappedBy = "user_id", cascade = CascadeType.ALL,
              orphanRemoval = true)
   private List<Comments> comments;
+
+  @OneToMany(mappedBy = "user_id", cascade = CascadeType.ALL,
+             orphanRemoval = true)
+  private List<FlaggedPosts> flaggedPosts;
 }
