e20350e5bacb69c18a2712f2b4a40cbabfc248ad
Yash <yashrtalele@gmail.com>
2024-02-28 03:04:01 +0530

    Added Blogs DTO, Service, Repository and APIs and added findById to UserService

---
+++ src/main/java/in/app/heal/aux/AuxBlogsDTO.java
@@ -0,0 +1,19 @@
+package in.app.heal.aux;
+
+import in.app.heal.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuxBlogsDTO {
+  private Integer blog_id;
+  private Integer user_id;
+  private String title;
+  private String description;
+  private java.util.Date post_date;
+}
