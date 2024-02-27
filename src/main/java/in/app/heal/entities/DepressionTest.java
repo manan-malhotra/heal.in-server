49f706b059afebc11f0651e0e673478761614f35
Yash <yashrtalele@gmail.com>
2024-02-27 20:45:24 +0530

    Added 3 Tests - Depression, Anxiety and Stress and did some changes in User.java

---
+++ src/main/java/in/app/heal/entities/DepressionTest.java
@@ -0,0 +1,35 @@
+package in.app.heal.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "depression_test")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class DepressionTest {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Integer id;
+
+  @Column(name = "question_1") private String question1;
+
+  @Column(name = "question_2") private String question2;
+
+  @Column(name = "question_3") private String question3;
+
+  @Column(name = "question_4") private String question4;
+
+  @Column(name = "question_5") private String question5;
+
+  @Column(name = "question_6") private String question6;
+
+  @Column(name = "question_7") private String question7;
+}
