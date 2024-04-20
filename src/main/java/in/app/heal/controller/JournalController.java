346f2ed636765dbb8bf283f93fe8f5e01d1350ac
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-04-20 12:30:38 +0530

    Moving journal business logic to service layer

--- src/main/java/in/app/heal/controller/JournalController.java
+++ src/main/java/in/app/heal/controller/JournalController.java
@@ -1,30 +1,18 @@
 package in.app.heal.controller;
 
-import in.app.heal.aux.AuxArticleDTO;
 import in.app.heal.aux.AuxJournalDTO;
-import in.app.heal.aux.AuxUserDTO;
 import in.app.heal.entities.JournalEntry;
-import in.app.heal.entities.User;
 import in.app.heal.entities.UserCredentials;
 import in.app.heal.service.JournalEntryService;
 import in.app.heal.service.TokenService;
 import in.app.heal.service.UserCredentialsService;
-import in.app.heal.service.UserService;
 import io.github.cdimascio.dotenv.Dotenv;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
-import java.util.Base64;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,51 +21,18 @@ import java.util.Optional;
 public class JournalController {
     Dotenv dotenv = Dotenv.load();
     @Autowired
-    private TokenService tokenService;
-
-    @Autowired
-    private UserCredentialsService userCredentialsService;
-
-    @Autowired
     private JournalEntryService journalEntryService;
     @PostMapping(path = "")
     public ResponseEntity<?> addJournalEntry(@RequestBody AuxJournalDTO auxJournalDTO, @RequestHeader(required = false)HttpHeaders headers){
-
-        JournalEntry journalEntry = new JournalEntry();
-        journalEntry.setTitle(auxJournalDTO.getTitle());
-        journalEntry.setDescription(auxJournalDTO.getDescription());
-        journalEntry.setEntry_date(new Date());
         String auth = headers.get("authorization").toString();
-        String token = tokenService.getToken(auth);
-        if(!token.isEmpty()){
-            try{
-                String email = tokenService.getEmailFromToken(token);
-                Optional<UserCredentials> userCredentialsOptional = userCredentialsService.findByEmail(email);
-                if(userCredentialsOptional.isPresent()){
-                    UserCredentials userCredentials = userCredentialsOptional.get();
-                    User user = userCredentials.getUser_id();
-                    journalEntry.setUser_id(user);
-                    journalEntryService.addJournalEntry(journalEntry);
-                    return new ResponseEntity<JournalEntry>(journalEntry,HttpStatus.OK);
-                }
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }catch (Exception e){
-                System.out.println(e);
-                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-            }
-        }else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return journalEntryService.addJournalEntry(auxJournalDTO, auth);
     }
 
     @PutMapping(path = "/edit")
     public ResponseEntity<?> editJournalEntry(@RequestBody AuxJournalDTO auxJournalEditDTO){
         Optional<JournalEntry> entryFound = journalEntryService.findByEntryId(auxJournalEditDTO.getEntryId());
         if(entryFound.isPresent()){
-            JournalEntry entry = entryFound.get();
-            entry.setDescription(auxJournalEditDTO.getDescription());
-            entry.setTitle(auxJournalEditDTO.getTitle());
-            journalEntryService.addJournalEntry(entry);
+            journalEntryService.ediJournalEntry(entryFound.get(), auxJournalEditDTO);
             return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
--- src/main/java/in/app/heal/service/JournalEntryService.java
+++ src/main/java/in/app/heal/service/JournalEntryService.java
@@ -1,18 +1,47 @@
 package in.app.heal.service;
 
+import in.app.heal.aux.AuxJournalDTO;
 import in.app.heal.entities.JournalEntry;
+import in.app.heal.entities.UserCredentials;
 import in.app.heal.repository.JournalEntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class JournalEntryService {
     @Autowired
+    TokenService tokenService;
+    @Autowired
+    UserCredentialsService userCredentialsService;
+    @Autowired
     JournalEntryRepository repository;
-
+    public ResponseEntity<?> addJournalEntry(AuxJournalDTO auxJournalDTO,String auth){
+        String token = tokenService.getToken(auth);
+        JournalEntry journalEntry = this.populateJournalEntry(auxJournalDTO);
+        if(!token.isEmpty()){
+            try{
+                String email = tokenService.getEmailFromToken(token);
+                Optional<UserCredentials> userCredentialsOptional = userCredentialsService.findByEmail(email);
+                if(userCredentialsOptional.isPresent()){
+                    this.populateJournalEntry(journalEntry, userCredentialsOptional.get());
+                    this.addJournalEntry(journalEntry);
+                    return new ResponseEntity<JournalEntry>(journalEntry,HttpStatus.OK);
+                }
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }catch (Exception e){
+                System.out.println(e);
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
     public void addJournalEntry(JournalEntry journalEntry){
         repository.save(journalEntry);
     }
@@ -26,5 +55,22 @@ public class JournalEntryService {
 
     public Optional<List<JournalEntry>> findAllByUserId(Integer userId){
         return repository.findAllByUserId(userId);
+    }
+
+    public JournalEntry populateJournalEntry(AuxJournalDTO auxJournalDTO){
+        JournalEntry journalEntry = new JournalEntry();
+        journalEntry.setTitle(auxJournalDTO.getTitle());
+        journalEntry.setDescription(auxJournalDTO.getDescription());
+        journalEntry.setEntry_date(new Date());
+        return journalEntry;
+    }
+    public JournalEntry populateJournalEntry(JournalEntry journalEntry, UserCredentials userCredentials){
+        journalEntry.setUser_id(userCredentials.getUser_id());
+        return journalEntry;
+    }
+    public JournalEntry ediJournalEntry(JournalEntry journalEntry, AuxJournalDTO auxJournalEditDTO){
+        journalEntry.setDescription(auxJournalEditDTO.getDescription());
+        journalEntry.setTitle(auxJournalEditDTO.getTitle());
+        return repository.save(journalEntry);
     }
 }
