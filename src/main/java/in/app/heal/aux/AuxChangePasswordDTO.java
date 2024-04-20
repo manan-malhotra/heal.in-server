bcde3f21441ffd382093c32aecb30bbe8be4474c
Yash <yashrtalele@gmail.com>
2024-04-21 01:47:07 +0530

    Added Change Password - dto, controller and service

---
+++ src/main/java/in/app/heal/aux/AuxChangePasswordDTO.java
@@ -0,0 +1,14 @@
+package in.app.heal.aux;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuxChangePasswordDTO {
+  private String email;
+  private String password;
+}
