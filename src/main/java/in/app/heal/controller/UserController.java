e213a2db0f9dec5a58d2334d66181e70c600ff52
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-04-22 17:34:14 +0530

    Removing unused imports

--- src/main/java/in/app/heal/controller/UserController.java
+++ src/main/java/in/app/heal/controller/UserController.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +19,6 @@ public class UserController {
   @Autowired private UserCredentialsService userCredentialsService;
   @Autowired private PublicQNAService publicQNAService;
   @Autowired private CommentService commentService;
-  PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
   Dotenv dotenv = Dotenv.load();
 
